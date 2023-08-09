c688ac6e6424f77bc0b50b3d4159c48ef9196ac5
Mobin-B <mobin827@outlook.com>
2023-08-09 04:07:24 +0330

    Some UI changes and add Database reader script.

--- Home/House_Script.js
+++ Home/House_Script.js
@@ -1,3 +1,38 @@
-function post(){
+
+
+var request = new XMLHttpRequest();
+request.open('GET', '../Database/Json/Posts.json');
+
+var posts = JSON.parse(request.responseText);
+
+request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+
+        var output;
+        for (var key in posts) {
+            output += '<article class="main_article not-selectable">';
+
+            output += '<header class="article_profile"><img src="../Database/Image/Profiles/Avatar.webp" alt="" class="article_avatar">';
+            output += '<h5 class="article_username">' + posts[key].username + '</h5>';
+
+            output += '<article class="moit"><p>' + posts[key].text + '</p></article>';
+
+            output += '<footer class="article_footer">';
+            output += '<i class="bi bi-hand-thumbs-up article_footer_items">&nbsp; ' + posts[key].like + '</i>';
+            output += '<i class="bi bi-hand-thumbs-down article_footer_items">&nbsp; ' + posts[key].dislike + '</i>';
+            output += '</footer>'
+
+            output += '</article><hr>';
+        }
+
+        document.getElementById(articles).innerHTML = output;
+
+    }
+}
+
+request.send();
+
+
+function post() {
     location = "https://mobin-b.github.io/Post/Post.html"
 }
