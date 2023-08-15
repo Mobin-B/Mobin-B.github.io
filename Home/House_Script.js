1be35249e3781bb81b4d81ba0fdf0de77177522f
Mobin-B <mobin827@outlook.com>
2023-08-15 20:31:16 +0330

    Test request command.

--- Home/House_Script.js
+++ Home/House_Script.js
@@ -1,34 +1,37 @@
 
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://mobin-b.github.io/Database/Json/Posts.json');
+request.open('GET', 'https://mobin-b.github.io/Database/Json/Posts.json', true);
 
-var posts = JSON.parse(request.responseText);
+console.log(request);
+console.log("----------------------#");
+console.log(request.responseText);
+// var posts = JSON.parse(request.responseText);
 
-request.onreadystatechange = function () {
-    if (request.readyState === 4 && request.status === 200) {
+// request.onreadystatechange = function () {
+//     if (request.readyState === 4 && request.status === 200) {
 
-        var output;
-        for (var key in posts) {
-            output += '<article class="main_article not-selectable">';
+//         var output;
+//         for (var key in posts) {
+//             output += '<article class="main_article not-selectable">';
 
-            output += '<header class="article_profile"><img src="../Database/Image/Profiles/Avatar.webp" alt="" class="article_avatar">';
-            output += '<h5 class="article_username">' + posts[key].username + '</h5>';
+//             output += '<header class="article_profile"><img src="../Database/Image/Profiles/Avatar.webp" alt="" class="article_avatar">';
+//             output += '<h5 class="article_username">' + posts[key].username + '</h5>';
 
-            output += '<article class="moit"><p>' + posts[key].text + '</p></article>';
+//             output += '<article class="moit"><p>' + posts[key].text + '</p></article>';
 
-            output += '<footer class="article_footer">';
-            output += '<i class="bi bi-hand-thumbs-up article_footer_items">&nbsp; ' + posts[key].like + '</i>';
-            output += '<i class="bi bi-hand-thumbs-down article_footer_items">&nbsp; ' + posts[key].dislike + '</i>';
-            output += '</footer>'
+//             output += '<footer class="article_footer">';
+//             output += '<i class="bi bi-hand-thumbs-up article_footer_items">&nbsp; ' + posts[key].like + '</i>';
+//             output += '<i class="bi bi-hand-thumbs-down article_footer_items">&nbsp; ' + posts[key].dislike + '</i>';
+//             output += '</footer>'
 
-            output += '</article><hr>';
-        }
+//             output += '</article><hr>';
+//         }
 
-        document.getElementById(articles).innerHTML = output;
+//         document.getElementById(articles).innerHTML = output;
 
-    }
-}
+//     }
+// }
 
 request.send();
 
