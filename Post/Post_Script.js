408d2ae817dfd852df252c8c5f6fbdd8145d9c95
Mobin-B <mobin827@outlook.com>
2023-08-19 01:38:13 +0330

    Working on profiles selecting panel.

--- Post/Post_Script.js
+++ Post/Post_Script.js
@@ -1,3 +1,14 @@
-function home(){
+
+
+var characters = document.getElementsByTagName("img");
+console.log(characters);
+var output = "";
+var pictures_num = 17;
+for (var i = pictures_num; i >= 0; i--) {
+    output += '<label class="profile"><input type="radio" name="profile" checked><img src="https://mobin-b.github.io/Database/Image/Profiles/' + i + '.png" alt = "" class="profile_img" ></label > ';
+}
+
+
+function home() {
     location = "https://mobin-b.github.io/Home/House.html"
 }
