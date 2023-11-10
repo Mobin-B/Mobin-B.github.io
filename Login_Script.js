dce198f8e7b7b21f91e1bd4ffde18edd56624807
Mobin-B <mobin827@outlook.com>
2023-11-10 17:59:05 +0330

    Add option to play 2048.

--- Login_Script.js
+++ Login_Script.js
@@ -1,6 +1,8 @@
 function login() {
     if (document.getElementById("text").value == "counter") {
         location = "Plugins/Counter.html"
+    } else if (document.getElementById("text").value == "2048") {
+        location = "Game/2048.html"
     } else {
         location = "Home/House.html"
     }
